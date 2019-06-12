8d6c982467ccb31204a83d842080f77cb1482b04
Ignacio <nachootegui@hotmail.com>
2019-06-12 10:49:58 -0300

    Disparador

---
+++ DisparosEnElSpace/Assets/Scripts/Disparador.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparador : MonoBehaviour
+{
+
+    public GameObject Bullet;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("right shift"))
+        {
+            print("Entro el if");
+            Invoke("Disparar", 2f);
+        }
+    }
+
+    void Disparar()
+    {
+        Instantiate(Bullet, transform.position, transform.rotation);
+        
+    }
+    
+}
